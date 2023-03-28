b3f5d6326d5adbb513b10a3d425691391db13911
Brian Zavala <brianzav23@gmail.com>
2023-03-28 13:02:35 -0300

    correção de erros e modificação de títulos e imagens

--- lib/features/home/view/home_page.dart
+++ lib/features/home/view/home_page.dart
@@ -48,34 +48,37 @@ class _HomeViewState extends State<HomeView> {
                     name: 'Comidas',
                     url:
                         'https://www.istoedinheiro.com.br/wp-content/uploads/sites/17/2019/08/din1135-sustenta5.jpg'),
+                Category(
+                    name: 'Lanches',
+                    url:
+                        'https://img.freepik.com/fotos-premium/classico-cheeseburger-com-carne-de-queijo-bacon-tomate-cebola-e-alface-isolado-no-fundo-branco_183587-963.jpg'),
+                Category(
+                    name: 'Pizza',
+                    url: 'https://www.pngmart.com/files/1/Pepperoni-Pizza.png'),
               ],
             ),
             PromocoesView(
               promotionList: [
                 Promotion(
-                    name: 'Hamburguer',
+                    name: 'Promo 1',
                     url:
                         'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSykY9ZDB3QYHKVYoc5q7yXsFKlT7vtYrs-Mg&usqp=CAU'),
                 Promotion(
-                    name: 'Pizza',
+                    name: 'Promo 2',
                     url:
                         'https://static-images.ifood.com.br/image/upload/t_medium/pratos/a3e32426-7659-484b-9309-bf3249bcaf62/202106301645_1VSY_i.jpg'),
                 Promotion(
-                    name: 'Lanches',
+                    name: 'Promo 3',
                     url:
-                        'https://www.circuitodenoticias.com.br/up/img/1621351538864.jpeg'),
+                        'https://blog.consumer.com.br/wp-content/uploads/2020/02/ifood-loop.jpg'),
                 Promotion(
-                    name: 'Hamburguer',
+                    name: 'Promo 4',
                     url:
-                        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSykY9ZDB3QYHKVYoc5q7yXsFKlT7vtYrs-Mg&usqp=CAU'),
+                        'https://i1.wp.com/surgiu.com.br/wp-content/uploads/2018/08/Mc-iFood-mcdiafeliz180824_121012.jpg?resize=1280%2C720'),
                 Promotion(
-                    name: 'Hamburguer',
+                    name: 'Promo 5',
                     url:
-                        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSykY9ZDB3QYHKVYoc5q7yXsFKlT7vtYrs-Mg&usqp=CAU'),
-                Promotion(
-                    name: 'Hamburguer',
-                    url:
-                        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSykY9ZDB3QYHKVYoc5q7yXsFKlT7vtYrs-Mg&usqp=CAU'),
+                        'https://gkpb.com.br/wp-content/uploads/2019/08/cropped-ifood-oferecera-pratos-de-R-1-para-novos-usuarios-geek-publicitario.jpg.png'),
               ],
             )
           ],
@@ -130,7 +133,7 @@ class CategoriesView extends StatelessWidget {
             style: TextStyle(fontSize: 18),
           ),
           SizedBox(
-            height: 200,
+            height: 180,
             child: ListView.builder(
               shrinkWrap: true,
               scrollDirection: Axis.horizontal,
@@ -140,8 +143,8 @@ class CategoriesView extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     SizedBox(
-                      height: 100,
-                      width: 100,
+                      height: 80,
+                      width: 150,
                       child: Image.network(
                         categoryList[index2].url,
                         errorBuilder: (context, error, stackTrace) {
@@ -158,10 +161,10 @@ class CategoriesView extends StatelessWidget {
                     ),
                     const SizedBox(height: 16),
                     Text(
-                      categoryList[index2].name.toLowerCase(),
+                      categoryList[index2].name,
                       style: const TextStyle(
                         fontSize: 14,
-                        color: Colors.blueAccent,
+                        color: Color.fromARGB(255, 71, 75, 80),
                       ),
                     ),
                   ],
@@ -195,7 +198,7 @@ class PromocoesView extends StatelessWidget {
             style: TextStyle(fontSize: 18),
           ),
           SizedBox(
-            height: 200,
+            height: 150,
             child: ListView.builder(
               shrinkWrap: true,
               scrollDirection: Axis.horizontal,
@@ -206,7 +209,7 @@ class PromocoesView extends StatelessWidget {
                   children: [
                     SizedBox(
                       height: 100,
-                      width: 100,
+                      width: 150,
                       child: Image.network(
                         promotionList[index].url,
                         errorBuilder: (context, error, stackTrace) {
@@ -215,18 +218,18 @@ class PromocoesView extends StatelessWidget {
                             alignment: Alignment.center,
                             child: const Text(
                               'Whoops!',
-                              style: TextStyle(fontSize: 30),
+                              style: TextStyle(fontSize: 20),
                             ),
                           );
                         },
                       ),
                     ),
-                    const SizedBox(height: 16),
+                    const SizedBox(height: 5),
                     Text(
-                      promotionList[index].name.toLowerCase(),
+                      promotionList[index].name,
                       style: const TextStyle(
                         fontSize: 14,
-                        color: Colors.blueAccent,
+                        color: Color.fromARGB(255, 71, 75, 80),
                       ),
                     ),
                   ],
